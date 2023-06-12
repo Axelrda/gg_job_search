a1ef69efcd722485b592fffd1a0a9dd011c9d95f
Axelrda <axelruffierdesaimes@gmail.com>
2023-06-12 13:29:52 +0000

    .

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -1,36 +1,95 @@
 import pandas as pd
 import numpy as np
+import unicodedata
 import re
 import requests
 
 
+def get_salary_currency(salaries):
 
-def get_salary_col(df):
+  cond_period = [
     
-    # Extract salary from extensions col
-    df['extracted_salary'] = pd.Series(df.extensions.str.split(', ', expand=True)[1])
-       
-    # Replace non-salary values to np.NaN
-    df['extracted_salary'].replace(["'à plein temps']", "'à temps partiel']", "'stage']", "'prestataire']"], np.NaN, inplace=True)
+    salaries.str.contains(r'\€|eur|euro|euros'),
+    salaries.str.contains(r'\$us|\$'),
+    salaries.str.contains(r'\£'),
+
+  ]
+
+  choices_period = [
     
-    # Convert null values to Nothing_found to make parsing easier
-    df.at[df.extracted_salary.isnull(), 'extracted_salary'] = "nothing_found"
-    
-    return df
+    'euros',
+    'us dollars',
+    'sterling pounds'
+
+  ]
+
+  salary_currency = pd.Series(np.select(cond_period, choices_period, default=np.NaN))
+
+  return salary_currency
 
 
-def get_og_salary_currency(df):
+def get_salary_period(salaries):
+
+  cond_period = [
     
-    df['og_salary_currency'] = df.extracted_salary.str.extract(r'(\€|\$us)', flags=re.IGNORECASE)
+    salaries.str.contains(r'an|year'),
+    salaries.str.contains(r'mois'),
+    salaries.str.contains(r'par semaine'),
+    salaries.str.contains(r'jour|tjm|per day'),
+    salaries.str.contains(r'heure|par h|/h\b')
+
+  ]
+
+  choices_period = [
     
-    return df
+    'an',
+    'mois',
+    'semaine',
+    'jour',
+    'heure',
+
+  ]
+
+  salary_period = pd.Series(np.select(cond_period, choices_period, default=np.NaN))
+
+  return salary_period
 
 
-def get_og_salary_period(df):
+
+def get_salary_status(salaries):
+
+  cond_status = [
     
-    df['og_salary_period'] = df.extracted_salary.str.extract(r'(\ban\b|\bmois\b|\bjour\b)', flags=re.IGNORECASE)
+     salaries.str.contains(r'net'),
+     salaries.str.contains(r'brut'),
+  
+  ]
+
+  choices_status = [
     
-    return df
+    'net',
+    'brut'
+
+  ]
+
+  salary_status = pd.Series(np.select(cond_status, choices_status, default='brut'))
+
+  return salary_status
+
+
+
+def rm_non_digits(x):
+
+    if x is None:
+        return None
+    if isinstance(x, str):
+        x = ''.join([c for c in x if c is not None and (c.isdigit() or c == '.')])
+        if len(x) == 0:
+            return None
+        else:
+            return float(x)
+    else:
+        return None
     
 
 def clean_salary(df):
@@ -261,7 +320,7 @@ def convert_salary_period(df):
     # Return the modified dataframe
     return df
     
-def salary_prepro(df):
+def processing_extensions_salaries(df):
     
     df = get_salary_col(df)
     df = get_og_salary_period(df)
@@ -272,3 +331,126 @@ def salary_prepro(df):
     df = convert_salary_currency(df)
     
     return df
+
+
+def lowercase_and_remove_accents(df):
+
+  # lowering cases
+  df = df.applymap(lambda x: x.lower() if type(x) == str else x )
+
+  # removing accents
+  def remove_accents(text):
+      normalized = unicodedata.normalize('NFKD', text)
+      without_accents = [c for c in normalized if not unicodedata.combining(c)]
+      return ''.join(without_accents)
+
+  df = df.applymap(lambda x: remove_accents(x) if isinstance(x, str) else x)
+
+  return df
+
+
+def basic_cleaning(df):
+
+  #fill null values
+  df.fillna('na', inplace=True)
+
+  # removing duplicated records
+  df.drop_duplicates(['description'], inplace=True)
+
+  # reset index
+  df = df.reset_index(drop=True)
+
+  return df
+  
+
+def matching_cols(df):
+
+  # define mismatch value masks
+  mismatch_postedat = df.posted_at.str.slice(0,6) != "il y a"
+  mismatch_jobid = df.job_id.str.slice(0,6) != "eyjqb2"
+  mismatch_extensions = df.extensions.str.slice(0,2) != "['"
+  mismatch_schedule = (df.schedule_type != "a plein temps") & (df.schedule_type != "stage") & (df.schedule_type != "prestataire") & (df.schedule_type != "a temps partiel")
+  mismatch_datetime = df.date_time.str.slice(0,2) != "20"
+
+  # shift every column from 1 position starting at thumbnail col
+  matched_df = df[mismatch_datetime & mismatch_schedule & mismatch_extensions & mismatch_jobid & mismatch_postedat].iloc[:,-7:].shift(axis=1, periods=1).fillna('na')
+
+  # replace og data w/ shifted values
+  df.loc[mismatch_datetime & mismatch_schedule & mismatch_extensions & mismatch_jobid & mismatch_postedat, df.columns[-7:]] = matched_df.copy().values
+
+  # replace og data w/ shifted values
+  df.loc[mismatch_datetime & mismatch_schedule & mismatch_extensions & mismatch_jobid & mismatch_postedat, df.columns[-7:]] = matched_df.copy().values
+
+
+  mismatch_thumbnail = df.loc[df['job_id'].str.startswith(('https:')), df.columns[7:-4]].shift(axis=1, periods=1)
+
+  mismatch_thumbnail['thumbnail'] = df.loc[df['job_id'].str.startswith(('https:')), 'job_id']
+
+  df.loc[df['job_id'].str.startswith(('https:')), df.columns[7:-4]] = mismatch_thumbnail.copy().values
+
+
+  # get location col values in job_id col + shift dataframe
+  mismatch_cities = df.loc[~df['job_id'].str.startswith(('eyjqb2', 'il y a', 'https:', 'na'))].loc[:, 'location': 'job_id'].shift(axis=1, periods=1)
+
+  # assign these values to location col
+  mismatch_cities['location'] = df.loc[~df['job_id'].str.startswith(('eyjqb2', 'il y a', 'https:', 'na')), 'job_id']
+
+  # replace og data w/ newly arranged values
+  df.loc[~df['job_id'].str.startswith(('eyjqb2', 'il y a', 'https:', 'na')), df.columns[2:-4]] = mismatch_cities.copy().values
+
+
+  extensions_in_thumbnail = df[df.thumbnail.str.slice(0,6) == "['il y"].loc[:, 'thumbnail': 'job_id'].shift(axis=1, periods=1).fillna('na')
+
+  df.loc[df.thumbnail.str.slice(0,6) == "['il y", df.columns[7:-4]] = extensions_in_thumbnail.copy().values
+
+
+  mismatch_postedat = df.loc[~df.posted_at.str.startswith(('il y a', 'na'))][['posted_at', 'schedule_type']]
+
+  mismatch_postedat['posted_at'], mismatch_postedat['schedule_type'] = mismatch_postedat['schedule_type'], mismatch_postedat['posted_at']
+
+  df.loc[~df.posted_at.str.startswith(('il y a', 'na')), df.columns[-4:-2]] = mismatch_postedat.copy().values
+
+  return df
+
+  
+
+def basic_prepro(df):
+
+  df = lowercase_and_remove_accents(df)
+  df = basic_cleaning(df)
+  df = matching_cols(df)
+
+  return df 
+
+
+def get_last_records(df):
+    """
+    Returns the last records from a DataFrame based on the date_time column.
+    
+    Args:
+        df (pandas.DataFrame): The DataFrame to extract the last records from.
+        
+    Returns:
+        pandas.DataFrame: The last records from the DataFrame.
+    """
+    # Select the records where the date_time is equal to the maximum date_time value
+    last_data = df[pd.to_datetime(df.date_time).dt.date == pd.to_datetime(df.date_time).dt.date.max()]
+    
+    return last_data
+
+
+def get_lang_records(df, language):
+    """
+    Returns the records from a DataFrame that match a specific language.
+    
+    Args:
+        df (pandas.DataFrame): The DataFrame to filter.
+        language (str): The language to filter by.
+        
+    Returns:
+        pandas.DataFrame: The filtered DataFrame containing only the records with the specified language.
+    """
+    # Filter the DataFrame based on the lang_labels column
+    filtered_df = df[df['lang_labels'] == language]
+    
+    return filtered_df
