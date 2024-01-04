145726eac34e4f144e637eb5b82a0f260c00927c
Axel RUFFIER DES AIMES <axelruffierdesaimes@gmail.com>
2024-01-04 18:50:18 +0100

    adds modifs to SEARCH_QUERIES variable in params.py

--- jobsearch/params.py
+++ jobsearch/params.py
@@ -1,11 +1,8 @@
-from dotenv import load_dotenv
 import os
 
-# Load the .env file
-load_dotenv()
 
 API_KEY = os.environ.get("API_KEY")
-SEARCH_QUERIES = os.environ.get("SEARCH_QUERIES")
+SEARCH_QUERIES = os.environ.get("SEARCH_QUERIES").split(',')
 COUNTRY_CODE = os.environ.get("COUNTRY_CODE")
 TARGET_TYPE = os.environ.get("TARGET_TYPE")
 DB_PATH = os.environ.get("DB_PATH")
