ddfe212b5153ff1f00562a831abf12ba79fc712a
Axel RUFFIER DES AIMES <axelruffierdesaimes@gmail.com>
2024-01-05 20:49:16 +0100

    Modify dag start date and schedule

--- dag/dag.py
+++ dag/dag.py
@@ -5,26 +5,26 @@ from airflow.operators.bash import BashOperator
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime.datetime(2023, 6, 16, tzinfo=pytz.timezone("Europe/Paris")),
+    'start_date': datetime.datetime(2024, 1, 3, tzinfo=pytz.timezone("Europe/Paris")),
     'retries': 1,
 }
 
 dag = DAG(
-    'gg_jobs_search',
+    'ggjobsearch',
     default_args=default_args,
     description='',
-    schedule='00 22 * * *',  # Run daily at 10pm
+    schedule='05 23 * * *',  # Run daily at 10pm
 )
 
 scrape_jobs_task = BashOperator(
     task_id='run_scraping_script',
-    bash_command='/home/axel/anaconda3/envs/gg_job_search/bin/python /home/axel/Documents/gg_job_search/scripts/scraping_jobs.py',
+    bash_command='/home/axelus/anaconda3/envs/ggjobsearch/bin/python /home/axelus/data_science_projects/gg_job_search/jobsearch/scraping_jobs.py',
     dag=dag,
 )
 
 sql_deduplicate_task = BashOperator(
     task_id='run_sql_deduplication_script',
-    bash_command='/home/axel/anaconda3/envs/gg_job_search/bin/python /home/axel/Documents/gg_job_search/src/cleaning/sql_deduplication.py',
+    bash_command='/home/axelus/anaconda3/envs/ggjobsearch/bin/python /home/axelus/data_science_projects/gg_job_search/jobsearch/sql_deduplication.py',
     dag=dag
 )
 
